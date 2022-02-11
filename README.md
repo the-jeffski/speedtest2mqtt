7a21e28b40adc3614de1b179f51f220735f94d02
Jeff <the-jeffski@gmail.com>
2022-02-11 13:46:26 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # speedtest2mqtt
-Shell script  to run OOKLA speedtest CLI and post to MQTT
+Shell script  to run OOKLA speedtest CLI and post to MQTT for use in Home Assistant dashboard.
+
+Requires Mosquitto Client installed
+
+Accepts licences, runs and pushes results to MQTT topic.
+
+Only tested on *nix
---
+++ speedtest2mqtt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Only edit the following setup lines with correct login information
+
+# Mosquitto Broker Details
+
+mqttuser=
+mqttpassword=
+mqttbroker=xx.xx.xx.xx
+mqttport=1883
+homeassistant=false
+
+# DO NOT ALTER ANYTHING BELOW THIS LINE
+
+speedtest_result=$( /usr/bin/speedtest --format=json --accept-license --accept-gdpr)
+echo "**********************************************************************************************"
+echo $speedtest_result
+
+download=$(echo "$speedtest_result" | jq '.download."bandwidth"')
+upload=$(echo "$speedtest_result" | jq '.upload."bandwidth"')
+ping_server=$(echo "$speedtest_result" | jq '.ping."latency"')
+url_test=$(echo "$speedtest_result" | jq '.result."url"' | tr -d \")
+time_stamp=$(echo "$speedtest_result" | jq '.timestamp' | tr -d \")
+server_name=$(echo "$speedtest_result" | jq '.server."name"')
+server_location=$(echo "$speedtest_result" | jq '.server."location"')
+server_id=$(echo "$speedtest_result" | jq '.server."id"')
+
+# Result output for log file purposes (set in cron job)
+echo $download $upload $ping_server $time_run $server_name 
+
+download=$(printf %.2f "$((10**3 * $download*8/10**6))e-3")
+upload=$(printf %.2f "$((10**3 * $upload*8/10**6))e-3")
+ping_server=$(printf %.3f "$ping_server")
+echo $download $upload $ping_server $time_run
+
+
+speedteststring='{"type":'$download',"attributes":{"time_stamp":"'"$time_stamp"'","ping":'$ping_server',"download":'$download',"upload":'$upload',"server_name":'$server_name',"server_id":'$server_id',"server_location":'$server_location'}}'
+echo $speedteststring
+echo "**********************************************************************************************"
+
+# Check server ID in case its on the blocklist
+
+# Publish to MQTT Broker
+
+mosquitto_pub -h $mqttbroker -p $mqttport -u "$mqttuser" -P "$mqttpassword" -t speedtest/$HOSTNAME/status -m "$speedteststring" -r
