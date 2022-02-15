8d73ae33d11b6e7c67d27067be5a2e775b71a76c
The-Jeffski <63499439+the-jeffski@users.noreply.github.com>
2022-02-15 13:50:45 +0000

    Updated with a loop to skip servers

--- speedtest2mqtt.sh
+++ speedtest2mqtt.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 ## Speedtest2Mqtt Shell script
+## Runs the Testing using the OOKLA CLI and posts to MQTT. Skips the results if a known bad server (must be set below).
 
 # Login information For MQTT Broker
 mqttuser=
 mqttpassword=
-mqttbroker=xx.xx.xx.xx
+mqttbroker=192.168.0.8
 mqttport=1883
 
 # Other Settings
 
 homeassistant=false
 
+# Check server ID in case its on the blocklist and if so skip posting results and have another try if retry is enabled
+badservers='34881 34931'
+
 # Run SpeedTest and get variables from Json
 
-speedtest_result=$( /usr/bin/speedtest --format=json --accept-license --accept-gdpr)
+speedtest_result=$(/usr/bin/speedtest --format=json --accept-license --accept-gdpr)
 echo "**********************************************************************************************"
 echo $speedtest_result
 
@@ -28,20 +32,25 @@ server_location=$(echo "$speedtest_result" | jq '.server."location"')
 server_id=$(echo "$speedtest_result" | jq '.server."id"')
 
 # Result output for log file purposes (set in cron job)
-echo $download $upload $ping_server $time_run $server_name 
+echo $download $upload $ping_server $time_run $server_name
 
-download=$(printf %.2f "$((10**3 * $download*8/10**6))e-3")
-upload=$(printf %.2f "$((10**3 * $upload*8/10**6))e-3")
+download=$(printf %.2f "$((10 ** 3 * $download * 8 / 10 ** 6))e-3")
+upload=$(printf %.2f "$((10 ** 3 * $upload * 8 / 10 ** 6))e-3")
 ping_server=$(printf %.3f "$ping_server")
 echo $download $upload $ping_server $time_run
-
 
 speedteststring='{"type":'$download',"attributes":{"time_stamp":"'"$time_stamp"'","ping":'$ping_server',"download":'$download',"upload":'$upload',"server_name":'$server_name',"server_id":'$server_id',"server_location":'$server_location'}}'
 echo $speedteststring
 echo "**********************************************************************************************"
 
-# Check server ID in case its on the blocklist
+# If server in bad list then ignore the test
+if [[ "$badservers" == *"$server_id"* ]]
 
-# Publish to MQTT Broker
-
-mosquitto_pub -h $mqttbroker -p $mqttport -u "$mqttuser" -P "$mqttpassword" -t speedtest/$HOSTNAME/status -m "$speedteststring" -r
+then
+    # Do nothing
+    echo "Test matches bad server so skip posting results"
+else
+    # Publish to MQTT Broker
+    echo "$server_id"
+    #mosquitto_pub -h $mqttbroker -p $mqttport -u "$mqttuser" -P "$mqttpassword" -t speedtest/$HOSTNAME/status -m "$speedteststring" -r
+fi
